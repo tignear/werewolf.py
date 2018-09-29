5081089783db3f1a52b27da46f9a0a0472946ae5
tignear <tignear+m@gmail.com>
2018-09-29 13:39:06 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name='werewolf',
+    version='0.1.0',
+    description='werewolf',
+    url='https://github.com/tignear/werewolf.py',
+    author='tignear',
+    author_email='tignear+m@gmail.com',
+    license='',
+    keywords='',
+    packages=[
+        "werewolf",
+    ],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
